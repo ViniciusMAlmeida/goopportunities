46be86f072578d9643a67804be590a91253bc7ad
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2024-06-19 18:55:31 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/ViniciusMAlmeida/goopportunities/router"
+import (
+	"github.com/ViniciusMAlmeida/goopportunities/config"
+	"github.com/ViniciusMAlmeida/goopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }
